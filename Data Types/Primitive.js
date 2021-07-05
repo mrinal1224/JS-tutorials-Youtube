03a1be6942c030568992118c93aafdbb9c27e120
mrinal1224 <iammrinalkc@gmail.com>
2021-07-06 02:00:25 +0530

    data types

---
+++ Data Types/Primitive.js
@@ -0,0 +1,30 @@
+//Number
+
+let a = 2;
+console.log(a ,typeof(a))
+
+//String
+
+let myString = "My name is Mrinal"
+
+console.log(myString,typeof(myString))
+
+//Boolean
+
+let willRain = false;
+
+console.log(willRain,typeof(willRain))
+
+//undefined
+
+let c;
+console.log(c,typeof(c))
+
+//Null
+
+let nullVariable = null;
+
+console.log(nullVariable , typeof(nullVariable))
+
+
+
---
+++ Data Types/Reference.js
@@ -0,0 +1,13 @@
+//object
+let personDetail ={
+    name : "Adam",
+    age : 30,
+    phone : 12345678
+}
+
+console.log(personDetail)
+
+//arrays
+
+let array = [1 ,2 ,3, "Steve" , true , null ];
+console.log(array)
