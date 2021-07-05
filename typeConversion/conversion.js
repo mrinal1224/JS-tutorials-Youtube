b00aa7c56f6fa263e6ff0db9f014714136d007c3
mrinal1224 <iammrinalkc@gmail.com>
2021-07-06 03:39:28 +0530

    type conversion and coercion

---
+++ typeConversion/conversion.js
@@ -0,0 +1,11 @@
+//let pincode = String(491441)
+//console.log(pincode , typeof pincode)
+
+//let myString = Number("14")
+//console.log(myString , typeof myString)
+
+//Boolean
+
+let myString = Boolean(1)
+console.log(myString , typeof(myString))
+
